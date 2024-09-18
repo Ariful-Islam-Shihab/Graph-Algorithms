0f94095cc06e2e41f40ac2bbacd7b0347e1c4f71
Ariful-Islam-Shihab <137407580+Ariful-Islam-Shihab@users.noreply.github.com>
2024-09-19 00:16:56 +0600

    Add files via upload

---
+++ Floyd_warshall.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int V;
+    cin>>V;
+    long long int adjMat[V][V];
+    for(int i=0;i<V;i++){
+        for(int j=0;j<V;j++){
+            cin>>adjMat[i][j];
+            if(adjMat[i][j]==-1)
+                adjMat[i][j]=INT_MAX;
+        }
+    }
+    for(int k=0;k<V;k++){
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(adjMat[i][k]+adjMat[k][j]<adjMat[i][j])
+                    adjMat[i][j]=adjMat[i][k]+adjMat[k][j];
+            }
+        }
+    }
+    for(int i=0;i<V;i++){
+        for(int j=0;j<V;j++){
+            cout<<adjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
